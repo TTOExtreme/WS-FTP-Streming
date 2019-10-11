f03073cdacdb696d8b6ad3314af72f6c28963efe
Lucas <cmarottol@hotmalil.com>
2019-10-11 15:17:44 -0300

    Layout change

--- web/home/js/index.js
+++ web/home/js/index.js
@@ -21,9 +21,21 @@ function InitHome() {
                     let last = data.data.pop();
                     last = JSON.parse(last);
                     div.innerHTML = createCast(last.title, last.descricao, last.title.replace(new RegExp(" ", "g"), "-"), last.title.replace(new RegExp(" ", "g"), "-"), "last_cast_div", "col-1", "last_cast");
+                    let id = 0;
                     data.data.forEach(e => {
                         e = JSON.parse(e);
-                        div.innerHTML += createCast(e.title, e.descricao, e.title.replace(new RegExp(" ", "g"), "-"), e.title.replace(new RegExp(" ", "g"), "-"));
+                        if (id < 12) {
+                            if (id < 2) {
+                                div.innerHTML += createCast(e.title, e.descricao, e.title.replace(new RegExp(" ", "g"), "-"), e.title.replace(new RegExp(" ", "g"), "-"));
+                            } else {
+                                if (id < 2) {
+                                    div.innerHTML += createCast(e.title, e.descricao, e.title.replace(new RegExp(" ", "g"), "-"), e.title.replace(new RegExp(" ", "g"), "-"), "old_cast_div", "col-3", "old_cast");
+                                } else {
+                                    div.innerHTML += createListCast(e.title, e.descricao, e.title.replace(new RegExp(" ", "g"), "-"), e.title.replace(new RegExp(" ", "g"), "-"));
+                                }
+                            }
+                            id++;
+                        }
                     });
                 }
             }
@@ -51,6 +63,25 @@ function createCast(title, desc, img, audio, clas1 = "old_cast_div", clas2 = "co
         "<div class=\"play_on_img\" onclick=\"playfile('" + audio + "')\">" +
         "<img src=\"./home/img/play_cast.png\" width=\"80px\">" +
         "</div>" +
+        "</div>";
+}
+
+function createListCast(title, desc, img, audio) {
+    return "" +
+        "<div class=\"list_cast_div col-4\">" +
+        "<div class=\"list_cast_img\"style=\"background-image: url('./data/" + img + "/img.png'); \" >" +
+        "<div class=\"play_on_img\" onclick=\"playfile('" + audio + "')\">" +
+        "<img src=\"./home/img/play_cast.png\" width=\"80px\">" +
+        "</div>" +
+        "</div>" +
+        "<div class='list_cast_title'><b>" + title + "</b></div>" +
+        "<div class=\"list_cast\" >" +
+        "<table style=\"width: calc(100% - 25px);\">" +
+        "<tr>" +
+        "<td>" + desc + "</td>" +
+        "</tr>" +
+        "</table>" +
+        "</div >" +
         "</div>";
 }
 
